c7f675f6e40f0fc9036fa5272b788037fafd6262
Emerson Paiva <emersonaraujopaiva@gmail.com>
2021-06-21 17:57:11 -0300

    scripts: add webpack prod configs

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -5,15 +5,17 @@ import { StylesProvider } from '@material-ui/core/styles'
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
-const App = () => (
-  <StylesProvider>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/pricing' component={Pricing} />
-        <Route path='/' component={Landing} />
-      </Switch>
-    </BrowserRouter>
-  </StylesProvider>
-)
+const App = () => {
+  return (
+    <StylesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={'/pricing'} component={Pricing} />
+          <Route path={'/'} component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
+  )
+  }
 
 export default App;
