398aed4ff1814d0e45deee80e659151eb01cbafa
Emerson Paiva <emersonaraujopaiva@gmail.com>
2021-06-22 08:51:51 -0300

    refactor: app return

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -5,17 +5,15 @@ import { StylesProvider } from '@material-ui/core/styles'
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
-const App = () => {
-  return (
-    <StylesProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={'/pricing'} component={Pricing} />
-          <Route path={'/'} component={Landing} />
-        </Switch>
-      </BrowserRouter>
-    </StylesProvider>
-  )
-  }
+const App = () => (
+  <StylesProvider>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={'/pricing'} component={Pricing} />
+        <Route path={'/'} component={Landing} />
+      </Switch>
+    </BrowserRouter>
+  </StylesProvider>
+)
 
 export default App;
